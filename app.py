964e57a26c525a09a354d4e7a98bd1553109f018
keertiojha18@gmail.com <keertiojha18@gmail.com>
2026-08-12 00:04:37 +0000

    resolving error in app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def sync_weather_alerts():
             all_alerts.extend(nws_client.fetch_alert_for_cities(cities))
         if state_codes:
             logger.info(f"fetching alerts for{len(state_codes)} states codes")
-            all_alerts.extend(nws_client.fetch_alert_for_cities(state_codes))
+            all_alerts.extend(nws_client.fetch_alerts_for_cities(state_codes))
         if not all_alerts:
             return jsonify(
                 {
